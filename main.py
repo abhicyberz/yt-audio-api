bd5934b11e75dcfe6d3d2cec7d61f7621fc0cbef
abhicyberz <premiumrdx01@gmail.com>
2026-09-13 09:12:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -170,6 +170,6 @@ def get_video_stream(url: str = Query(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main.py:app", host="0.0.0.0", port=port, reload=False)
                         
